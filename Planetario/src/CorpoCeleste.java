810f1d3f67532c196cb132f451f62f13d7c305b3
yuhang <51709956+yuhang23@users.noreply.github.com>
2021-04-08 21:31:29 +0200

    prova1

--- Planetario/src/CorpoCeleste.java
+++ Planetario/src/CorpoCeleste.java
@@ -2,9 +2,13 @@
 public class CorpoCeleste {
 
 	private String nome;
+	private double massa;
+	private Punto punto;
 	
-	public CorpoCeleste(String nome) {
+	public CorpoCeleste(String nome, double massa, Punto punto) {
 		this.nome = nome;
+		this.massa = massa;
+		this.punto = punto;
 	}
 	
 }
--- Planetario/src/Punto.java
+++ Planetario/src/Punto.java
@@ -1,4 +1,12 @@
 
 public class Punto {
 
+	private int x;
+	private int y;
+	
+	public Punto(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 }
